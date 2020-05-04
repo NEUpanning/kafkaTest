2445c1edeafa62e0a5a7ab2e4fc38ed6e3c18f49
panning <emmning@163.com>
2020-05-04 15:13:18 +0800

    hhhh

--- src/main/java/producer/MyProducer.java
+++ src/main/java/producer/MyProducer.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.io.*;
 import java.util.Hashtable;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -32,11 +33,13 @@ public class MyProducer {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         //生产者对象
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-
-        for (int i = 0; i < 4; i++) {
-            producer.send(new ProducerRecord<>("first","INFO"+i ));
+//
+//
+        for (int i = 0; i < 10; i++) {
+            producer.send(new ProducerRecord<>("first","pi10mr,WT02287,2016/1/1 0:10,0,10.02,0,0,127.62,0,30.62,28.56,-3.11,2.19,22.25,0,0,0,0,0,0,0,0,-0.54,0.36,9815620,0,0,0,102,1,WT02287" ));
             System.out.println("lllll");
         }
+        //producer.send(new ProducerRecord<>("first","WT02287","pi10mr,WT02287,2016/1/1 0:10,0,10.02,0,0,127.62,0,30.62,28.56,-3.11,2.19,22.25,0,0,0,0,0,0,0,0,-0.54,0.36,9815620,0,0,0,102,1,WT02287" ));
         producer.close();
     }
 }
